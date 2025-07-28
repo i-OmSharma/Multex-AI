92d4f8f200eca319c69e2eaf7db4745793557baf
i-OmSharma <sharmaom1201@gmail.com>
2025-07-28 22:47:59 +0530

    Multex setup

---
+++ app/api-gateway/src/index.ts
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+import app from './app.js';
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`API Gateway running on http://localhost:${PORT}`);
+})
+
+
+
